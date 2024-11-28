efd8e1938108450f1848424afaffeac3b8107f09
Tae young Kim <tyk0707@gmail.com>
2024-11-28 12:23:28 +0900

    1230

--- Login/app/build.gradle.kts
+++ Login/app/build.gradle.kts
@@ -33,9 +33,22 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    implementation (libs.v2.all) //전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation (libs.v2.user) //카카오 로그인 API 모듈
+    implementation (libs.v2.share) //카카오톡 공유 API 모듈
+    implementation (libs.v2.talk) //카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation (libs.v2.friend) //피커 API 모듈
+    implementation (libs.v2.navi) //카카오내비 API 모듈
+    implementation (libs.v2.cert) //카카오톡 인증 서비스 API 모듈
+
+    implementation (libs.lottie)  //로티 라이브러리
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- Login/app/src/main/java/com/example/login/LoginActivity.kt
+++ Login/app/src/main/java/com/example/login/LoginActivity.kt
@@ -5,20 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.login.databinding.ActivityMainBinding
-import com.kakao.sdk.common.KakaoSdk
+import com.example.login.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Kakao SDK 초기화
-        KakaoSdk.init(this, "{NATIVE_APP_KEY}")
-
     }
 }
